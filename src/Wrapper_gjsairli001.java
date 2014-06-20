1c1cafb30584244e92fc4d53b29818659691d973
zhangyanchao <zhangyanchao@xinhuanet.com>
2014-06-20 17:32:59 +0800

    finish

--- src/Wrapper_gjsairli001.java
+++ src/Wrapper_gjsairli001.java
@@ -69,21 +69,50 @@ public class Wrapper_gjsairli001 implements QunarCrawler {
 
 	@Override
 	public BookingResult getBookingInfo(FlightSearchParam arg0) {
-		String bookingUrlPre = "https://www.luxair.lu/cms/luxair.php";
+		String bookingUrlPre = "http://book.liat.com/Select.aspx";
 		BookingResult bookingResult = new BookingResult();
 
 		BookingInfo bookingInfo = new BookingInfo();
 		bookingInfo.setAction(bookingUrlPre);
-		bookingInfo.setMethod("get");
+		bookingInfo.setMethod("post");
+		// 获取年月日
+		String[] dates = arg0.getDepDate().split("-");
+		String[] redates = arg0.getRetDate().split("-");
 		Map<String, String> map = new LinkedHashMap<String, String>();
-		map.put("p", "EN,17958,,,,,");
-		String lap = String
-				.format("PRD,en,2,ECONOMY_BUSINESS_LASTMINUTE,%s,%s,%s,%s,false,O,1,0,0,0,WWW_LUXAIR_LU,true,true,true,false,false,true,,,",
-						arg0.getDep(), arg0.getArr(), arg0.getDepDate().replaceAll("-", ""), arg0.getRetDate()
-								.replaceAll("-", ""));
-		map.put("lap", lap);
-		// map.put("lgBookButton", "Book Now");
-		// map.put("Referer", "https://www.luxair.lu/cms/page?p=en,17652,,,,,");
+
+		map.put("AvailabilitySearchInputSelectView$ButtonSubmit", "");
+		map.put("AvailabilitySearchInputSelectView$TextBoxMarketDestination2", arg0.getDep());
+		map.put("AvailabilitySearchInputSelectView$TextBoxMarketOrigin2", arg0.getArr());
+		map.put("AvailabilitySearchInputSelectView$promocode", "");
+		map.put("ControlGroupPriceAndConverterSelectView$CurrConvertSelectViewAjax$DropDownListConvCurr", "");
+		map.put("ControlGroupSelectView$AvailabilityInputSelectView$market1", "");
+		map.put("ControlGroupSelectView$AvailabilityInputSelectView$market2", "");
+		// new NameValuePair("ControlGroupSelectView$AvailabilityInputSelectView$market1",
+		// "0~V~~V12~0950~~1~X|LI~ 370~ ~~BGI~08/19/2014 08:20~SLU~08/19/2014 09:05~"),
+		map.put("__EVENTARGUMENT", "");
+		map.put("__EVENTTARGET", "");
+		map.put("destinationStation2", arg0.getDep());
+		map.put("originStation2", arg0.getArr());
+		map.put("pageToken", "");
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketDateRange1", "0|0");
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketDateRange2", "0|0");
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketDay1", dates[2]);
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketDay2", redates[2]);
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketMonth1", dates[0] + "-" + dates[1]);
+		map.put("AvailabilitySearchInputSelectView$DropDownListMarketMonth2", redates[0] + "-" + redates[1]);
+		map.put("AvailabilitySearchInputSelectView$DropDownListPassengerType_ADS", "0");
+		map.put("AvailabilitySearchInputSelectView$DropDownListPassengerType_ADT", "1");
+		map.put("AvailabilitySearchInputSelectView$DropDownListPassengerType_CHD", "0");
+		map.put("AvailabilitySearchInputSelectView$RadioButtonMarketStructure", "RoundTrip");
+		map.put("AvailabilitySearchInputSelectView$TextBoxMarketDestination1", arg0.getArr());
+		map.put("AvailabilitySearchInputSelectView$TextBoxMarketOrigin1", arg0.getDep());
+		map.put("__VIEWSTATE", "/wEPDwUBMGRkbUFOpZl3yd/NgMo6CxihRbceiZ4=");
+		map.put("date_picker", arg0.getDepDate());
+		map.put("date_picker", arg0.getRetDate());
+		map.put("destinationStation1", arg0.getArr());
+		map.put("originStation1", arg0.getDep());
+		map.put("query", "Search");
+
 		bookingInfo.setInputs(map);
 		bookingResult.setData(bookingInfo);
 		bookingResult.setRet(true);
